fbeb394887a8cc2c1d39b5d15173eecdd63c84e2
beratgulsen <71222198+beratgulsen@users.noreply.github.com>
2022-09-23 22:40:34 +0300

    Update Header.jsx

--- components/Static/Header.jsx
+++ components/Static/Header.jsx
@@ -13,7 +13,7 @@ const MobileNavbar = ({ open, setOpen, NavItems }) => {
 
                 <div className="flex justify-between border-b border-white/20 items-center px-5 py-4">
                     <div className="flex items-center">
-                        <img width="32" className="rounded-full icon-glow" src="https://cdn.discordapp.com/avatars/931685972733751337/d6ba1bdc2e2bc14082c1acba8df91124.webp" />
+                        <img width="32" className="rounded-full icon-glow" src="https://cdn.discordapp.com/avatars/931685972733751337/4d7afb365c69106d7b674770012f432b.webp" />
                         <p className=" font-extrabold ml-1 text-2xl">Moon Plus</p>
                     </div>
                     <button onClick={() => setOpen(!open)}><i className="cursor-pointer fa fa-times text-xl mr-2" /></button>
@@ -76,7 +76,7 @@ const Header = ({ $, NavItems }) => {
                 <div className="max-w-7xl px-5 mx-auto py-5 flex items-center justify-between">
                     <div className="flex items-center space-x-6">
                         <div className="flex items-center space-x-3">
-                            <img src="https://cdn.discordapp.com/avatars/931685972733751337/d6ba1bdc2e2bc14082c1acba8df91124.webp" 
+                            <img src="https://cdn.discordapp.com/avatars/931685972733751337/4d7afb365c69106d7b674770012f432b.webp" 
                                 className={`
                                     rounded-full 
                                 `}
